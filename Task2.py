487f85b189f69725497439f9c3ef4ed8d63b1002
andrey.baev <andrey.baev@am-intech.ru>
2023-11-25 02:30:02 +0300

    do Task2

--- Task2.py
+++ Task2.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from pandasql import sqldf
+#from pandasql import sqldf
 
-file = "student-mat.csv"
+file = "Kotelenko\student-mat.csv"#поменять!
 
 df = pd.read_csv(file)
 
@@ -23,9 +23,12 @@ FROM df
 #print(result) это сделано чтобы можно было понять, какие столбцы удаленны, так как ответы одинаковые drop_duplicates не нужен 
 
 int_cols = [col for col in df.columns if df.dtypes[col] == 'int64']
-print(int_cols)
 
-q_low = df["var1"].quantile(0.05)
-q_high = df["var1"].quantile(0.95)
-
+for var in int_cols:
+    q_low = df[int_cols].quantile(0.05)
+    print(q_low)
+    q_high = df[int_cols].quantile(0.95)
+    print(q_high)    
+    data = data.loc[(data["var1"] > q_low) & (data["var1"] < q_high)]    
+    
 
