6ea9520b45ccd504e59abff31d6850d2eb05af8c
Баев Андрей Анатольевич <andrey.baev@am-intech.ru>
2023-11-25 01:33:12 +0300

    do

--- Task2.py
+++ Task2.py
@@ -1,3 +1,16 @@
 import pandas as pd
 from pandasql import sqldf
 
+file = "student-mat.csv"
+
+df = pd.read_csv(file)
+
+
+query = """
+SELECT *
+FROM df
+"""
+
+result = sqldf(query)
+print(result)
+
