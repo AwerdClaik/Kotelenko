578bc0f64e57d315472dd0b80e25cbbcd7fd357d
Баев Андрей Анатольевич <andrey.baev@am-intech.ru>
2023-11-25 02:53:08 +0300

    do Task2

--- Task2.py
+++ Task2.py
@@ -1,7 +1,9 @@
 import pandas as pd
 #from pandasql import sqldf
+import numpy as np
 
-file = "Kotelenko\student-mat.csv"#поменять!
+
+file = "Kotelenko/student-mat.csv"#поменять!
 
 df = pd.read_csv(file)
 
@@ -22,13 +24,20 @@ FROM df
 #result = sqldf(sql)
 #print(result) это сделано чтобы можно было понять, какие столбцы удаленны, так как ответы одинаковые drop_duplicates не нужен 
 
-int_cols = [col for col in df.columns if df.dtypes[col] == 'int64']
+numeric_data = df.select_dtypes(include=[np.number])
 
-for var in int_cols:
-    q_low = df[int_cols].quantile(0.05)
+outliers = {}
+for col in numeric_data.columns:
+    outliers[col] = np.sum(np.abs(numeric_data[col] - np.mean(numeric_data[col])) > 3 * np.std(numeric_data[col]))
+
+percent_outliers = {}
+for col in numeric_data.columns:
+    percent_outliers[col] = (outliers[col] / numeric_data[col].shape[0]) * 100
+
+
+for col, percent in percent_outliers.items():
+    percent = percent/100
+    q_low = df[col].quantile(percent)
     print(q_low)
-    q_high = df[int_cols].quantile(0.95)
+    q_high = df[col].quantile(0.95)
     print(q_high)    
-    data = data.loc[(data["var1"] > q_low) & (data["var1"] < q_high)]    
-    
-
