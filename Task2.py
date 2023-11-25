1bd37d9393f77e55189df3a5bbd1afe6449c923b
Баев Андрей Анатольевич <andrey.baev@am-intech.ru>
2023-11-25 04:17:17 +0300

    сделал убирание экстремумов,остановился на матрице

--- Task2.py
+++ Task2.py
@@ -1,7 +1,8 @@
 import pandas as pd
 #from pandasql import sqldf
 import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 file = "Kotelenko/student-mat.csv"#поменять!
 
@@ -9,12 +10,12 @@ df = pd.read_csv(file)
 
 df.info()
 
-print(df.isnull().sum())
+print(df.isnull().sum()) #узнаем кол-во Null
 
-sql = """
-SELECT * 
-FROM df
-"""
+#ql = """
+#SELECT * 
+#FROM df
+#"""
 
 #result = sqldf(sql)
 #print(result)
@@ -26,8 +27,26 @@ FROM df
 
 numeric_data = df.select_dtypes(include=[np.number])
 
+outliers = {}
+for col in numeric_data.columns:
+    outliers[col] = np.sum(np.abs(numeric_data[col] - np.mean(numeric_data[col])) > 3 * np.std(numeric_data[col]))#мы смотрим по утроенному стандартному отклонению
+
+for col, count in outliers.items():
+    print(f"Столбец {col} содержит {count} выбросов.")#считаем вбросы чтобы понимать нужно ли их убирать
+
+
 for col in numeric_data.columns:
     upper_limit = df[col].mean() + 3 * df[col].std()
-    print(f"{col} {upper_limit}")
+    #print(f"{col} upper_limit: {upper_limit}")
     lower_limit = df[col].mean() - 3 * df[col].std()    
-    print(f"{col} {lower_limit}")      
+    #print(f"{col} lower_limit: {lower_limit}")      
+    df = df.loc[(df[col] > lower_limit) & (df[col] < upper_limit)]
+
+df.info()
+
+numeric_data = df.select_dtypes(include=[np.number])
+corr_matrix = numeric_data.corr()
+
+
+sns.heatmap(corr_matrix, annot=True, cmap="coolwarm")
+plt.show()
